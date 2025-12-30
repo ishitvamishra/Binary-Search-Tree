110724ec0db81905fdf3cbfbfed437b561f63264
Ishitva Narayan Mishra <149973191+ishitvamishra@users.noreply.github.com>
2025-12-30 20:17:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floor in BST/floor-in-bst.java
@@ -0,0 +1,34 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public static int floor(Node root, int x) {
+        // code here
+        int floor =-1;
+        while(root != null){
+            if(x == root.data){
+                floor = root.data;
+                return floor;
+            }
+        
+            if(x > root.data){
+                floor = root.data;
+                root = root.right;
+            }
+            else{
+                root = root.left;
+            }
+        }
+        return floor;
+    }
+}
